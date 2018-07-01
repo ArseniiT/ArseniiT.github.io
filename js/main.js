d6eedadf7cd4a7a1863d8640cf074ff27cc3d250
Arsenii Tolmachev <arseniit@gmail.com>
2018-07-01 18:06:54 -0400

    made portfolio like slider

--- js/main.js
+++ js/main.js
@@ -19,3 +19,15 @@ function slowScroll (id) {
   return false;
 }
 
+// Slick slider
+
+$(document).ready(function(){
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true
+  });
+});
+
